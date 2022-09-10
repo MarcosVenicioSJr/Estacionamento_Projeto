24799c672f07bc7f475974ff3dd16df08f2b7f0a
Marcos Venicio <marcos.venicio1230@hotmail.com>
2022-09-10 11:52:48 -0300

    Instanciando classe na main

--- Principal.cs
+++ Principal.cs
@@ -3,6 +3,12 @@ class Principal
 {
     static void Main(string[] args)
     {
+        Console.Write("Digite o valor do estacionamento: ");
+        decimal valueInitial = decimal.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o valor por hora: ");
+        decimal valueHour = decimal.Parse(Console.ReadLine());
+
 
     }
 }
