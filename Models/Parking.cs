3d40c2e135afe8d81188a9509b4b5acfb6bf24cc
Marcos Venicio <marcos.venicio1230@hotmail.com>
2022-09-10 11:31:36 -0300

    Metodo de adição de carro criado. Iniciando metodo de remoção

--- Models/Parking.cs
+++ Models/Parking.cs
@@ -6,5 +6,31 @@ namespace EstacionamentoDIO.Models
         private decimal valueHour;
         private List<string> cars = new List<string>();
 
+        public Parking(decimal valueInitial, decimal valueHour)
+        {
+            this.valueHour = valueHour;
+            this.valueInitial = valueInitial;
+        }
+
+        public void addVehicle()
+        {
+            Console.Clear();
+            Console.WriteLine("Bem vindo ao Estacionamento.");
+            Console.WriteLine("Digite aqui sua placa: ");
+            string board = Console.ReadLine();
+
+            if (board != null)
+            {
+                cars.Add(board);
+            }
+            else
+            {
+                Console.WriteLine("Placa inválida. Digite novamente sua placa: ");
+                addVehicle();
+            }
+
+
+        }
+
     }
 }
