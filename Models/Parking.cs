71b5e97b3749092f0cc203285d20fa5e594bc5d9
Marcos Venicio <marcos.venicio1230@hotmail.com>
2022-09-10 11:24:52 -0300

    Criando metodos da classe parking

---
+++ Models/Parking.cs
@@ -0,0 +1,10 @@
+namespace EstacionamentoDIO.Models
+{
+    public class Parking
+    {
+        private decimal valueInitial;
+        private decimal valueHour;
+        private List<string> cars = new List<string>();
+
+    }
+}
---
+++ Principal.cs
@@ -0,0 +1,9 @@
+using EstacionamentoDIO.Models;
+class Principal
+{
+    static void Main(string[] args)
+    {
+        // Display the number of command line arguments.
+        Console.WriteLine(args.Length);
+    }
+}
